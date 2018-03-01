70311a10b6678d969da568816715b31421d45510
Malik Albeik <abd.beik@me.com>
2018-03-02 00:30:02 +0300

    added the alternate attribute to all images

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + " restaurant's image";
   li.append(image);
 
   const name = document.createElement('h1');
