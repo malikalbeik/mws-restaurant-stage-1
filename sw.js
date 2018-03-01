d37e98ca5843bd2134f5cfe49b549dd2bba92dbf
Malik Albeik <abd.beik@me.com>
2018-03-01 23:01:22 +0300

    moved the service worker to the main directory and registered the service worker and added caching to the website so that it works offline

---
+++ sw.js
@@ -0,0 +1,62 @@
+var CACHE_NAME = 'my-site-cache-v1';
+
+var urlsToCache = [
+  '/',
+  '/restaurant.html?id=1',
+  '/restaurant.html?id=2',
+  '/restaurant.html?id=3',
+  '/restaurant.html?id=4',
+  '/restaurant.html?id=5',
+  '/restaurant.html?id=6',
+  '/restaurant.html?id=7',
+  '/restaurant.html?id=8',
+  '/restaurant.html?id=9',
+  '/restaurant.html?id=10',
+  '/js/main.js',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+  '/js/dbhelper.js',
+  '/js/restaurant_info.js',
+  '/sw.js',
+  '/js/service-worker/index.js',
+  '/css/responsive.css'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    }).catch(function(err) {
+      console.log(err);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+        console.log('response given from caches')
+      }
+
+      return fetch(event.request);
+    }).catch(function(err) {
+      console.log(err);
+    })
+  );
+});
